2ed8c4ac0dcfc270b508b174b3993df460ce4f6e
Anna <ann.adonts02@gmail.com>
2023-09-26 13:31:20 +0400

    Change the url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('programmers/', include('programmers.urls')),  
+    path('', include('programmers.urls')),  
 ]
 
